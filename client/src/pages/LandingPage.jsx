99b3cf19afe42d53ce69b303f2e778c903290378
laxit_khanpara <laxitkhanpara3646@gmail.com>
2024-06-04 16:10:47 +0530

    pages and component

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const LandingPage = () => {
+  return (
+    <>
+        <Navbar/>
+        <Header/>
+        lol
+        <Footer/>
+        
+        
+    </>
+  )
+}
+
+export default LandingPage
