da3e6a93e3874176f532985149a72b3631e77c84
laxit_khanpara <laxitkhanpara3646@gmail.com>
2024-06-05 15:09:01 +0530

    style.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';  
-import '../public/style.css'
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
