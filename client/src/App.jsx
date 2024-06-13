785292063b14f956c165f0f30bbb64b13e673299
Herohasu <harsh.ajay2017@gmail.com>
2024-06-13 17:52:12 +0530

    2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';  
-//import '../public/style.css'
 import HomePage from './pages/HomePage/HomePage';
 import Services from './pages/Services/Services';
 import About from './pages/About/About';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Routes>
