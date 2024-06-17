907da39f3ba8e16e5e7d0644f8d1a49a0a2ec389
Aagam <aagamshah1754@gmail.com>
2024-06-18 00:34:32 +0530

    new

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-
-const Contact = () => {
+import './Contact.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Navbar from '../Navbar/Navbar';
+function Contact () {
   return (
     <div>
-      
+      <Navbar/>
+      <div className="contact-page-area section-gap">
+        <div className="contact-container">
+          <div className="row">
+            <div className='col-lg-4 d-flex flex-column address-wrap'>
+              <div className='single-contact-address d-flex flex-row'>
+                <div className="contact-details">
+                  <h5>Amusment Park</h5>
+                </div>
+                <div className="single-contact-address d-flex flex-row">
+                <div className="contact-details">
+							    <h5>1234567890</h5>
+							    <p>Mon to Sat 9am to 5pm</p>
+						      </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    
   )
 }
 
