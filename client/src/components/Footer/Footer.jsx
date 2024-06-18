bd9bd3dfe7419a50b4fc64cc4c7dcdd5c9b7b72e
Aagam <aagamshah1754@gmail.com>
2024-06-19 00:31:42 +0530

    Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
-import './Footer.css'
-import { Link } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React, { useState } from 'react';
+import './Carousel.css';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+import { FaInstagram, FaFacebook, FaYoutube } from 'react-icons/fa';
+
+const testimonials = [
+    {
+        name: 'Maria Kate',
+        profession: 'Photographer',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti nesciunt sint eligendi reprehenderit reiciendis, quibusdam illo, beatae quia fugit consequatur laudantium velit magnam error. Consectetur distinctio fugit doloremque.',
+        image: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp',
+        rating: 4.5,
+    },
+    {
+        name: 'John Doe',
+        profession: 'Web Developer',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti nesciunt sint eligendi reprehenderit reiciendis.',
+        image: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp',
+        rating: 4.0,
+    },
+    {
+        name: 'Anna Deynah',
+        profession: 'UX Designer',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti nesciunt sint eligendi reprehenderit reiciendis, quibusdam illo, beatae quia fugit consequatur laudantium velit magnam error. Consectetur distinctio fugit doloremque.',
+        image: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp',
+        rating: 4.5,
+    },
+];
+
+const Carousel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1));
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    return (
+        <div className="carousel-wrapper">
+            <div className="carousel">
+                {testimonials.map((testimonial, index) => (
+                    <div className={`carousel-item ${index === currentIndex ? 'active' : ''}`} key={index}>
+                        <img className="avatar" src={testimonial.image} alt="avatar" />
+                        <div className="carousel-content">
+                            <h5 className="name">{testimonial.name}</h5>
+                            <p className="profession">{testimonial.profession}</p>
+                            <p className="text">
+                                <span className="quote">“</span>
+                                {testimonial.text}
+                                <span className="quote">”</span>
+                            </p>
+                            <div className="stars">
+                                {[...Array(5)].map((_, i) => (
+                                    <span key={i} className={i < testimonial.rating ? 'star filled' : 'star'}>&#9733;</span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <button className="control-prev" onClick={handlePrev}>
+                &#9664;
+            </button>
+            <button className="control-next" onClick={handleNext}>
+                &#9654;
+            </button>
+        </div>
+    );
+};
 
 const Footer = () => {
     return (
@@ -16,7 +84,6 @@ const Footer = () => {
                         Visit Us Now!!
                         Give Your Feedback
                     </p>
-
                     <div className="input-area">
                         <form>
                             <input type="text" name='Feedback' placeholder='Your Feedback' className='footer-input' />
@@ -27,24 +94,33 @@ const Footer = () => {
                 <div className="footer-link">
                     <div className="footer-link-wrapper">
                         <div className="footer-link-items">
-                            <h2>SocialMedia</h2>
-                            <Link to='/'>Instagram</Link>
-                            <Link to='/'>Facebook</Link>
-                            <Link to='/'>YouTube</Link>
+                            <h2>Social Media</h2>
+                            <div className="footer-icons">
+                                <Link to='/'>
+                                    <FaInstagram />
+                                </Link>
+                                <Link to='/'>
+                                    <FaFacebook />
+                                </Link>
+                                <Link to='/'>
+                                    <FaYoutube />
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-                {/* <section className='social-media'>
-            <div className="social-media-wrap">
-                <div className="footer-logo">
-                    <Link className='social-logo'></Link>
-                </div>
-            </div>
-        </section> */}
             </div>
         </>
+    );
+};
 
-    )
-}
+const FooterApp = () => {
+    return (
+        <div>
+            <Carousel />
+            <Footer />
+        </div>
+    );
+};
 
-export default Footer
+export default FooterApp;
