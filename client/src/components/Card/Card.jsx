75e36e448afb0fe024df989d4a0be0b9c6f8ff2f
DhavalBavda <dhavalbavdas@gmail.com>
2024-06-18 22:07:32 +0530

    ac

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -13,7 +13,7 @@ const Card = () => {
       <div className="row g-3">
         <div className="col-12 col-md-6 col-lg-4">
           <div className="card">
-            <img src={fairSwing_card} alt="" class="card-img-top"/>
+            <img src={fairSwing_card} alt="" class="card-img-top" id="fair-swing"/>
             <div className="card-body">
               <h5 className="card-title">Fair Swing </h5>
                 <p class="card-text">A classic fairground ride where you can swing high in the air.</p>
@@ -24,7 +24,7 @@ const Card = () => {
 
         <div className="col-12 col-md-6 col-lg-4">
           <div className="card">
-            <img src={Rollercoster_card} alt="" class="card-img-top"/>
+            <img src={Rollercoster_card} alt="" class="card-img-top" id="roller-coaster"/>
             <div className="card-body">
               <h5 className="card-title">Roller Coaster</h5>
                 <p class="card-text">A thrilling ride that takes you up and down steep hills and sharp turns.</p>
@@ -35,7 +35,7 @@ const Card = () => {
 
         <div className="col-12 col-md-6 col-lg-4">
           <div className="card">
-            <img src={droptower_card} alt="" class="card-img-top"/>
+            <img src={droptower_card} alt="" class="card-img-top" id="drop-tower"/>
             <div className="card-body">
               <h5 className="card-title">Drop Tower</h5>
                 <p class="card-text">A thrill ride that lifts riders to great heights before releasing them into a controlled free fall.</p>
@@ -46,7 +46,7 @@ const Card = () => {
 
         <div className="col-12 col-md-6 col-lg-4">
           <div className="card">
-            <img src={ferriswheel_card} alt="" class="card-img-top"/>
+            <img src={ferriswheel_card} alt="" class="card-img-top" id="ferris-wheel"/>
             <div className="card-body">
               <h5 className="card-title">Ferris Wheel</h5>
                 <p class="card-text">A classic amusement park ride that rotates passengers in gondolas around a central axis, offering panoramic views.</p>
@@ -57,7 +57,7 @@ const Card = () => {
 
         <div className="col-12 col-md-6 col-lg-4">
           <div className="card">
-            <img src={bumpercar_card} alt="" class="card-img-top"/>
+            <img src={bumpercar_card} alt="" class="card-img-top" id="bumper-car"/>
             <div className="card-body">
               <h5 className="card-title">Bumpper Car</h5>
                 <p class="card-text">An interactive ride where drivers control electric vehicles that bump into each other in a designated area.</p>
@@ -68,7 +68,7 @@ const Card = () => {
 
         <div className="col-12 col-md-6 col-lg-4">
           <div className="card">
-            <img src={waterpark_card} alt="" class="card-img-top"/>
+            <img src={waterpark_card} alt="" class="card-img-top" id="water-park"/>
             <div className="card-body">
               <h5 className="card-title">Water Ride</h5>
                 <p class="card-text"> A type of amusement park attraction that involves water features, such as splashing, spraying, or even a full-on water slide experience.</p>
