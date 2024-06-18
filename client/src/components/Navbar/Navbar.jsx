46d95b8528b173a54a841d5f4f73674d5d846f19
DhavalBavda <dhavalbavdas@gmail.com>
2024-06-18 13:59:17 +0530

    ..

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './Navbar.css';
 import menuicon from "../../../public/image/menu-icon.png"
-import Logo from "/image/logo.png"
+import Logo from "/image/Logo.png"
 
 function Navbar() {
 
