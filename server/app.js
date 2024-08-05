ccf688f73be4266dd34aebb21a0e2afdbea6f465
laxit_khanpara <laxitkhanpara3646@gmail.com>
2024-08-05 15:13:11 +0530

    ,j

--- server/app.js
+++ server/app.js
@@ -1,77 +1,65 @@
 require('dotenv').config();
-port=process.env.PORT || 35000
-const express =require("express");
-const app=express();
+const express = require("express");
 const mongoose = require('mongoose');
-const path = require('path')
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser')
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const url = require('url');
 
-const filename = path.join(__dirname,__filename)
-const fileUrl = url.pathToFileURL(filename).href;
+const app = express();
+const port = process.env.PORT || 35000;
 
-
-//============================================================
+// Middleware
 app.use(bodyParser.json());
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(express.static(path.join(__dirname, "../client/dist")));
-app.use(express.static(path.join(__dirname, "/server/Public")));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// CORS configuration
 app.use(cors());
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-//   });
-//=================connection to the db===============
-mongoose.connect(process.env.MONGODB,{
+
+// Database connection
+mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() =>{
-    console.log("congo! connection is done!!!")
-}).catch((error)=>{
-    console.log("not connected",error);
+}).then(() => {
+    console.log("Connected to MongoDB!");
+}).catch((error) => {
+    console.log("MongoDB connection error:", error);
 });
 
-const AuthRouter= require("./Router/AuthRouter");
+// Routes
+const AuthRouter = require("./Router/AuthRouter");
 const { AdminUsers } = require('./model/AuthSchema');
-app.use("/",AuthRouter);
+app.use("/", AuthRouter);
 
-app.get((req,res)=>{
-    res.send("hello")
-})
-
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+});
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = await AdminUsers.findOne({ username });
-    console.log("sfs")
+
     if (!user) return res.status(400).send('Invalid credentials');
 
-    if (user){
-        console.log("accepted")
-    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).send('Invalid credentials');
 
-    // const isMatch = await bcrypt.compare(password, user.password);
-    // if (!isMatch) return res.status(400).send('Invalid credentials');
-
-    // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-
-    const token = jwt.sign({ username : user.Username }, process.env.JWT_SECRET);
-    console.log(token)
-    res.json({token});
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ token });
 });
 
-//==================server=========================
-app.listen(port,()=>{
-    try{
-        console.log(`connection is working on '${port}'`)
-    } catch(error){
+// Start server
+app.listen(port, () => {
+    try {
+        console.log(`Server running on port ${port}`);
+    } catch (error) {
         console.log(error);
     }
-})
-
-
+});
