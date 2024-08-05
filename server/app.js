7670b39017e58cf03a649617f99219587962e2fe
laxit_khanpara <laxitkhanpara3646@gmail.com>
2024-08-05 13:47:50 +0530

    d

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 //=================connection to the db===============
 mongoose.connect(process.env.MONGODB,{
