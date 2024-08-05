fbfee382304bb2b2d8770755149c560471cdc4e4
laxit_khanpara <laxitkhanpara3646@gmail.com>
2024-08-05 13:52:58 +0530

    mhvk

--- server/app.js
+++ server/app.js
@@ -19,12 +19,12 @@ const fileUrl = url.pathToFileURL(filename).href;
 app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
   });
 //=================connection to the db===============
 mongoose.connect(process.env.MONGODB,{
